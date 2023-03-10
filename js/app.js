b269984cc9ca8fa6ecee0b1c79da7c79c217d9bf
jakhongiralijonov <jakhongirweb@gmail.com>
2023-03-10 12:36:12 +0500

    cleared

--- js/app.js
+++ js/app.js
@@ -5,63 +5,81 @@ const listGroupTodo = document.getElementById("list-group-todo");
 const time = document.getElementById("time");
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
-const closeEl = document.getElementById('close')
+const closeEl = document.getElementById("close");
 /* time elements */
 const fullDay = document.getElementById("full-day");
 const year = document.getElementById("year");
 const day = document.getElementById("day");
 const month = document.getElementById("month");
-const hourEl  = document.getElementById('hour')
-const minuteEl  = document.getElementById('minute')
-const secondEl  = document.getElementById('second')
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
 
-let editItemId
+let editItemId;
+
 // check
 
-let todos = JSON.parse(localStorage.getItem('list')) ? JSON.parse(localStorage.getItem('list')) : []
+let todos = JSON.parse(localStorage.getItem("list"))
+  ? JSON.parse(localStorage.getItem("list"))
+  : [];
 
 // save to localstorage
-function setTodos(){
-  localStorage.setItem('list' , JSON.stringify(todos))
+function setTodos() {
+  localStorage.setItem("list", JSON.stringify(todos));
 }
+
 // time
-function getTime(){
-  const now = new Date()
-   const date = now.getDate() < 10 ? '0' + now.getDate() : now.getDate()
-   const month = now.getMonth()< 10 ? '0' + now.getMonth() : now.getMonth()
-   const year = now.getFullYear()
+function getTime() {
+  const now = new Date();
+  const date = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+  const month = now.getMonth() < 10 ? "0" + now.getMonth() : now.getMonth();
+  const year = now.getFullYear();
 
-   const hour = now.getHours()< 10 ? '0' + now.getHours() : now.getHours()
-   const minutes = now.getMinutes()< 10 ? '0' + now.getMinutes() : now.getMinutes()
-const seconds = now.getSeconds()< 10 ? '0' + now.getSeconds() : now.getSeconds()
-const monthNames = [
-  'January' , 'February' , 'March' , 'April' , ' May'  , 'June' , 'July' , 'August' , 'September' , 'October' , 'November' , 'December'
-]
+  const hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+  const minutes =
+    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  const seconds =
+    now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    " May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-const monthTitle = now.getMonth()
-fullDay.textContent =  `${date} ${ monthNames[monthTitle]} ${year}`
+  const monthTitle = now.getMonth();
+  fullDay.textContent = `${date} ${monthNames[monthTitle]} ${year}`;
 
-hourEl.textContent = hour
-minuteEl.textContent = minutes
-secondEl.textContent = seconds
+  hourEl.textContent = hour;
+  minuteEl.textContent = minutes;
+  secondEl.textContent = seconds;
 
-
- return (` ${hour}:${minutes}  ${date}.${ ('0'+( Number(month)+1)).slice(-2)}.${year}`);
+  return ` ${hour}:${minutes}  ${date}.${("0" + (Number(month) + 1)).slice(
+    -2
+  )}.${year}`;
 }
 
-setInterval(getTime , 1000)
+setInterval(getTime, 1000);
+
 // show todos
+if (todos.length) showTodos();
 
-
-
-if(todos.length) showTodos()
-
-function showTodos(){
-  listGroupTodo.textContent = ''
-  const todos = JSON.parse(localStorage.getItem('list'))
-  todos.forEach((item , i)=>{
+function showTodos() {
+  listGroupTodo.textContent = "";
+  const todos = JSON.parse(localStorage.getItem("list"));
+  todos.forEach((item, i) => {
     listGroupTodo.innerHTML += `
-    <li ondblclick='setCompleted(${i})' class="list-group-item d-flex justify-content-between ${item.completed  ? 'completed' : ''}">
+    <li ondblclick='setCompleted(${i})' class="list-group-item d-flex justify-content-between ${
+      item.completed ? "completed" : ""
+    }">
          ${item.text}
           <div class="todo-icons">
             <span class="opacity-50 me-2">${item.time}</span>
@@ -69,89 +87,91 @@ function showTodos(){
              <img onclick='deleteTodo(${i})' src="img/delete.svg" alt="delete image" width="25" height=" 25">
           </div>
         </li>
-    `
-  })
+    `;
+  });
 }
 
-// completed function
-function setCompleted(id){
-    const completeTodos  = todos.map((item, index)=>{
-      if( index == id ){
-        return {...item, completed : item.completed ? false : true}
-      } else {
-        return {...item}
-      }
-    })
+// listen form
 
-
-    todos = completeTodos
-    setTodos()
-    showTodos()
-}
-
-
-formCreate.addEventListener('click' , (e)=>{
-  e.preventDefault()
-  const todoText = formCreate['input-create'].value.trim()
-formCreate.reset()
-  if(todoText.length ){
-    todos.push({text: todoText , time: getTime() , completed: false})
-    setTodos()
-    showTodos()
-  } else showMessage('message-create' , 'Please create some text...')
-})
+formCreate.addEventListener("click", (e) => {
+  e.preventDefault();
+  const todoText = formCreate["input-create"].value.trim();
+  formCreate.reset();
+  if (todoText.length) {
+    todos.push({ text: todoText, time: getTime(), completed: false });
+    setTodos();
+    showTodos();
+  } else showMessage("message-create", "Please create some text...");
+});
 
 // show error function
 
-function showMessage(where , message){
-  document.getElementById(`${where}`).textContent = message
-    setTimeout(()=>{
-      document.getElementById(`${where}`).textContent= ''
-    } , 2500)
+function showMessage(where, message) {
+  document.getElementById(`${where}`).textContent = message;
+  setTimeout(() => {
+    document.getElementById(`${where}`).textContent = "";
+  }, 2500);
 }
-
 
 // delete todo
 
-function deleteTodo(id){
-  const deletedTodos = todos.filter((item, index)=>{
-    return index !== id 
-
-  })
-  todos = deletedTodos
-  setTodos()
-  showTodos()
+function deleteTodo(id) {
+  const deletedTodos = todos.filter((item, index) => {
+    return index !== id;
+  });
+  todos = deletedTodos;
+  setTodos();
+  showTodos();
 }
 
-formEdit.addEventListener('submit' , (e)=>{
-  e.preventDefault()
-  const todoText = formEdit['input-edit'].value.trim()
-  formEdit.reset()
-    if(todoText.length ){
-      todos.splice(editItemId , 1 ,{text: todoText , time: getTime() , completed: false})
-        setTodos()
-      showTodos()
-      closeModal()
-    } else showMessage('message-edit' , 'Please create some text...')
-})
+// completed function
+function setCompleted(id) {
+  const completeTodos = todos.map((item, index) => {
+    if (index == id) {
+      return { ...item, completed: item.completed ? false : true };
+    } else {
+      return { ...item };
+    }
+  });
 
-
-function editTodo(id){
-openModal()
-editItemId = id
+  todos = completeTodos;
+  setTodos();
+  showTodos();
 }
 
-function openModal(){
-modal.classList.remove('hidden')
-overlay.classList.remove('hidden')
-}
-document.addEventListener('keydown' , (e)=>{
-  if(e.which == 27) closeModal()
+// edit todo
 
-})
-overlay.addEventListener('click' , closeModal)
-closeEl.addEventListener('click' , closeModal)
-function closeModal(){
-  modal.classList.add('hidden')
-  overlay.classList.add('hidden')
+formEdit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoText = formEdit["input-edit"].value.trim();
+  formEdit.reset();
+  if (todoText.length) {
+    todos.splice(editItemId, 1, {
+      text: todoText,
+      time: getTime(),
+      completed: false,
+    });
+    setTodos();
+    showTodos();
+    closeModal();
+  } else showMessage("message-edit", "Please create some text...");
+});
+
+function editTodo(id) {
+  openModal();
+  editItemId = id;
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+document.addEventListener("keydown", (e) => {
+  if (e.which == 27) closeModal();
+});
+overlay.addEventListener("click", closeModal);
+closeEl.addEventListener("click", closeModal);
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
